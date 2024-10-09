2e6b70574f4b9673977df0ca39b4a238430a32f9
Doug Massay <dougmassay@users.noreply.github.com>
2024-10-09 09:17:40 -0400

    Update trans.sh [deploy]

--- trans.sh
+++ trans.sh
@@ -2,15 +2,15 @@
 
 # Create new translation strings messages.po file:
 
-find . -type f -name "*.py" | xgettext --output=messages.pot --omit-header -f -
+find . -type f -name "*.py" | xgettext --output=messages.pot --sort-by-file --omit-header -f -
 
 # Merge (update) with existing template and finsished translations files
 
-msgmerge -NU --backup=none ./translations/Trad.pot messages.pot
+msgmerge -NU --backup=none --sort-by-file ./translations/Trad.pot messages.pot
 for i in ./translations/*.po; do
     [ -f "$i" ] || break
     echo "$i"
-    msgmerge -NU --backup=none "$i" messages.pot
+    msgmerge -NU --backup=none --sort-by-file "$i" messages.pot
     mo_name=${i::-3}
     echo "$mo_name"
     msgfmt -o "$mo_name.mo" "$i"
